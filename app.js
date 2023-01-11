39216b268f7ef003689879192d56fbe91d7b0c56
StyloRK <122465936+StyloRK@users.noreply.github.com>
2023-01-11 16:52:29 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function posicaoativar(){
     goku = document.querySelector(".goku")
     goku.style.visibility = "hidden"
+
     setTimeout(() => {
         gokucarregando = document.getElementById("gokucarregando").style.visibility = "visible"
     }, 200);
-
 }
 
 function ativarkamehameha(){
@@ -17,18 +17,21 @@ function ativarkamehameha(){
 
 function ativar(){
     resetar1 = document.getElementById("resetar").style.visibility = "hidden"
+    kamehamehacharge = document.getElementById("kamehamehacharge").style.visibility = "hidden"
+    kamehamehacharge1 = document.getElementById("kamehamehacharge1").style.visibility = "visible"
     kamehameha = document.getElementById("kamehameha")
     kamehameha.play()
     kamehameha.currentTime = 0;
     setTimeout(() => {
         gokucarregando1 = document.getElementById("gokucarregando1").style.visibility = "hidden"
-        kamehamehacharge = document.getElementById("kamehamehacharge").style.visibility = "hidden"
+        kamehamehacharge1 = document.getElementById("kamehamehacharge1").style.visibility = "hidden"
         gokukamehameha = document.getElementById("gokukamehameha").style.visibility = "visible"
         kamehameha1 = document.querySelector(".kamehameha1")
         kamehameha1.style.visibility = "visible"
         kamehameha1.classList.add("animacao")
-        
+
     }, 8000);
+
     setTimeout(() => {
         resetar1 = document.getElementById("resetar").style.visibility = "visible"
     }, 10000);
@@ -45,4 +48,5 @@ function resetar(){
     kamehameha1.style.visibility = "hidden"
     kamehameha1.classList.remove("animacao")
     kamehameha = document.getElementById("kamehameha").pause()
+    
 }
